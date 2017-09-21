c04de17667615320065e44aed4ee9c9d761e50e4
NancyDLL <nano14@mac.com>
2017-09-21 13:16:07 -0500

    built the basic file.

--- BasicCard.js
+++ BasicCard.js
@@ -24,20 +24,42 @@ function BasicCard (basicFront, basicBack){
 	}
 };
 
-
-//prompt to ask questions and get responses
-inquirer.prompt([
-	{
-		type: "input",
-		name: "question1",
-		message: BasicCard.question1.front
+//when user types in node the game function starts
+if(process.argv[2] === "basic"){
+	runBasic();
 	}
-	]).then(function(userAnswer) {
-		console.log(userAnswer);
-});
+	//function for running the actual basic game
+	function runBasic(){
+		//set variable for card selection
+		var questionToAsk = new BasicCard(questions.queryList[i].basicFront, questions.queryList[i].basicBack);
+		//prompt to ask questions and get responses
+		inquirer.prompt([
+			{
+				type: "input",
+				name: "userAnswer",
+				message: questionToAsk.basicFront
+			}
+		//evaluate the responses entered by the user
+		]).then(function(response) {
+			if(response.userAnswer === questionToAsk.basicBack){
+				console.log("Correct answer.");
+			} else {
+				console.log("Incorrect answer. The correct answer is " + questionToAsk.basicBack);
+			}
+			//if condition to keep asking questions until all the way through the list
+			if(i<questions.queryList.length -1) {
+				i+=1;
+				runBasic();
+			}
+		});
+	}
 
 //Questions:
 //Is it better to use JSON(require) or JS(export) for question file? 
+//Which file will be called with node?
+//Does order of functions matter?
+//Should I count wins and losses? Instructions did not say to.
+//why didn't you select the type of prompt?
 //What does the module.export do, and why is it somethimes at the top of the file and sometimes the bottom?
 
 
